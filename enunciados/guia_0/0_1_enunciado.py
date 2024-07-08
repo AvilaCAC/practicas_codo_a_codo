83d064595ed2160f8ebe7f1b73cc54325f77c750
AvilaCAC <marcoantonio.avilatapia@bue.edu.ar>
2024-07-07 22:44:11 -0300

    guia_0: ejercicio_1

---
+++ enunciados/guia_0/0_1_enunciado.py
@@ -0,0 +1,10 @@
+""" Enunciado 0.1 
+Permitir que el usuario ingrese un nombre y mostrar un saludo usando ese nombre. 
++ Entrada: “Antonio” 
+Salida: “Hola Antonio”
+"""
+# Variables
+
+
+# Programa
+
---
+++ resueltos/guia_0/0_1_resuelto.py
@@ -0,0 +1,12 @@
+""" Enunciado 0.1 
+Permitir que el usuario ingrese un nombre y mostrar un saludo usando ese nombre. 
++ Entrada: “Antonio” 
+Salida: “Hola Antonio”
+"""
+# Variables
+nombre: str
+
+# Programa
+nombre = input("Ingrese un nombre: ")
+
+print("Hola " + nombre)
